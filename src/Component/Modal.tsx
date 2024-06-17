7044fb10d3f238fbd816485fc9ea8818e9ed21b8
서연 <155455327+seoyeon1123@users.noreply.github.com>
2024-06-17 14:48:44 +0900

    feat : modal 수정

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -3,14 +3,44 @@ import { useSetRecoilState } from 'recoil';
 import { toDoState } from '../atoms';
 import styled from 'styled-components';
 
+const ModalWrapper = styled.div`
+  position: fixed; /* 화면 고정 */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5); /* 배경 반투명 처리 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000; /* 다른 요소보다 위에 오도록 */
+`;
+
+const ModalContent = styled.div`
+  background: ${(props) => props.theme.cardColor};
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`;
+
 const Input = styled.input`
   border-style: none;
   background-color: ${(props) => props.theme.cardColor};
   border-bottom: 1px solid ${(props) => props.theme.boardColor};
+  color: ${(props) => props.theme.textColor};
 
   &:focus {
     outline: none;
   }
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => props.theme.boardColor};
+  margin-left: 10px;
+  color: ${(props) => props.theme.boardtextColor};
+  border-radius: 20px;
+  border: 1px solid ${(props) => props.theme.boardColor};
+  font-size: 15px;
 `;
 
 interface IModal {
@@ -49,12 +79,14 @@ const Modal = ({ toDoText, toDoId, boardId, onClose }: IModal) => {
 
   return (
     <>
-      <div>
-        <title>수정할 내용을 입력하세요</title>
-        <form onSubmit={onSubmit}>
-          <Input type="text" value={inputValue} onChange={onChange} />
-        </form>
-      </div>
+      <ModalWrapper onClick={onClose}>
+        <ModalContent onClick={(e) => e.stopPropagation()}>
+          <form onSubmit={onSubmit}>
+            <Input type="text" value={inputValue} onChange={onChange} />
+            <Button type="submit">확인</Button>
+          </form>
+        </ModalContent>
+      </ModalWrapper>
     </>
   );
 };
